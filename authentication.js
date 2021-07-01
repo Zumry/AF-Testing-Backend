263e5ae14fcf291a6014ce79c115d89523a0ef40
Mohamed Akeel <akeel1447@gmail.com>
2021-07-01 18:37:27 +0530

    logic error fixed in user authentication

--- authentication.js
+++ authentication.js
@@ -22,9 +22,9 @@ const verifyReviewer = async (ctx, next) =>{
         }
 
         if(User.type !== 'Reviewer'){
-            let err = new Error('You are not authorized to perform this operation!')
-            err.status = 403
-            return next(err)
+            let errorMessage = 'You are not authorized to perform this operation!'
+            const statusCode = 403
+            ctx.throw(statusCode,errorMessage)
         }
 
     await next();
@@ -44,9 +44,9 @@ const verifyResearcher = async (ctx, next) =>{
     }
 
     if(User.type !== 'Researcher'){
-        let err = new Error('You are not authorized to perform this operation!')
-        err.status = 403
-        return next(err)
+        let errorMessage = 'You are not authorized to perform this operation!'
+        const statusCode = 403
+        ctx.throw(statusCode,errorMessage)
     }
 
     await next();
@@ -66,9 +66,9 @@ const verifyWorkshopConductor = async (ctx, next) =>{
     }
 
     if(User.type !== 'WorkshopConductor'){
-        let err = new Error('You are not authorized to perform this operation!')
-        err.status = 403
-        return next(err)
+        let errorMessage = 'You are not authorized to perform this operation!'
+        const statusCode = 403
+        ctx.throw(statusCode,errorMessage)
     }
 
     await next();
@@ -88,9 +88,9 @@ const verifyEditor = async (ctx, next) =>{
     }
 
     if(User.type !== 'Editor'){
-        let err = new Error('You are not authorized to perform this operation!')
-        err.status = 403
-        return next(err)
+        let errorMessage = 'You are not authorized to perform this operation!'
+        const statusCode = 403
+        ctx.throw(statusCode,errorMessage)
     }
 
     await next();
@@ -110,9 +110,9 @@ const verifyAdmin = async (ctx, next) =>{
     }
 
     if(User.type !== 'Editor'){
-        let err = new Error('You are not authorized to perform this operation!')
-        err.status = 403
-        return next(err)
+        let errorMessage = 'You are not authorized to perform this operation!'
+        const statusCode = 403
+        ctx.throw(statusCode,errorMessage)
     }
 
     await next();
@@ -132,9 +132,9 @@ const verifyAttendees = async (ctx, next) =>{
     }
 
     if(User.type !== 'Attendee'){
-        let err = new Error('You are not authorized to perform this operation!')
-        err.status = 403
-        return next(err)
+        let errorMessage = 'You are not authorized to perform this operation!'
+        const statusCode = 403
+        ctx.throw(statusCode,errorMessage)
     }
 
     await next();
